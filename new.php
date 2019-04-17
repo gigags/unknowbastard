f668cbb1a05e6f436125636640e6f05d8c3af205
coolboy112233 <36572517+coolboy112233@users.noreply.github.com>
2019-04-17 11:03:59 +0530

    Create new.php

---
+++ new.php
@@ -0,0 +1,45 @@
+ <?php
+header("Access-Control-Allow-Origin: ".$_SERVER['HTTP_ORIGIN']);
+header("Access-Control-Allow-Methods: *");
+header("Access-Control-Allow-Credentials: true");
+
+ ?>
+<VAST version="2.0">
+<Ad id="2185">
+<InLine>
+<AdSystem>VS VAST 3.0</AdSystem>
+<AdTitle>
+<![CDATA[ Шоу «Холостяк». Новый сезон ]]>
+</AdTitle>
+<Impression id="Impression">
+<![CDATA[
+https://bit.ly/2Pe3iPB
+]]>
+</Impression>
+<TrackingEvents>
+              <Tracking event="skip">https://bit.ly/2ZhXpFF</Tracking>
+              <Tracking event="firstQuartile">http://example.com/pixel.gif</Tracking>
+              <Tracking event="midpoint">http://example.com/pixel.gif</Tracking>
+              <Tracking event="thirdQuartile">http://example.com/pixel.gif</Tracking>
+              <Tracking event="complete">http://example.com/pixel.gif</Tracking>
+              <Tracking event="pause">http://example.com/pixel.gif</Tracking>
+              <Tracking event="mute">http://example.com/pixel.gif</Tracking>
+              <Tracking event="fullscreen">http://example.com/pixel.gif</Tracking>
+</TrackingEvents>
+<Creatives>
+<Creative>
+<Linear>
+<AdParameters>
+<![CDATA[{"key":"a9b8231396d94e18dbf525c793a21a4d","id":"2185","skipTime":30,"skippable":true,"videoId":"tFWDfaEBDps","videoStartSec":0,"channelId":"UCsv_HGZUdIHUJCAsSnyMoRQ","delayForAction":"12","platformId":77,"campaignId":2185,"campaignTitle":"GASTREET \u041b\u0435\u0432\u0438\u0446\u043a\u043e\u0433\u043e \u0437\u0430\u0440\u0430\u0437\u0435\u043d! PRO \u0433\u043b\u0430\u0432\u043d\u043e\u0435 \u0440\u0435\u0441\u0442\u043e\u0440\u0430\u043d\u043d\u043e\u0435 \u0441\u043e\u0431\u044b\u0442\u0438\u0435 \u0441\u0442\u0440\u0430\u043d\u044b, \u043c\u044f\u0441\u043d\u043e\u0439 \u0441\u043a\u0430\u043d\u0434\u0430\u043b \u0432 Meat Puppets","duration":2083,"videoService":"youtube","rotator":false,"showShareBar":1,"hintText":"","ref":"","ref2":"https://t.co/UZtEpeGLqv","buster":"v10215","pixels":[],"buttons":[{"link":"https:\/\/yt.advmaker.su\/cnt\/redirect.php?a=2185&vid=bLxlRlM5Nms%3Fsub_confirmation%3D1","desc":"Go to the link","start_time":null,"end_time":null}],"playerHost":"https:\/\/ytimgg.com","time":1524832723.16,"did":"6c19a7b398fb8677b1664814dc1485e6"}]]>
+</AdParameters>
+<Duration>00:34:43</Duration>
+<MediaFiles>
+<MediaFile apiFramework="VPAID" delivery="progressive" type="application/javascript" width="640" height="360" maintainAspectRatio="1" bitrate="500"><![CDATA[ https://coolboy112233.github.io/unknowbastard/vpaidplayer_en.js ]]>
+</MediaFile>
+</MediaFiles>
+</Linear>
+</Creative>
+</Creatives>
+</InLine>
+</Ad>
+</VAST>
