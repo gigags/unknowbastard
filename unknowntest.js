6828bf2b36658da32831581e03279e752ef6944e
coolboy112233 <36572517+coolboy112233@users.noreply.github.com>
2019-06-14 01:27:10 +0530

    Update unknowntest.js

--- unknowntest.js
+++ unknowntest.js
@@ -615,6 +615,7 @@ function adscoreInit() {
 		return;
 	}
 	AdscoreInit("Qt0rAAAAAAAAFOimELjrFNnrsMxl1lq6zskuRME", {
+	sub_id: saved_arguments['source'],
 	callback: function(result) { validateSignature(result.signature)}
 	});
 }
@@ -637,7 +638,7 @@ function doStreaming(response) {
 		stream = document.createElement('iframe');
 		stream.setAttribute("style","height:"+cordinat.height+"px;width:"+cordinat.width+"px;border:0px;position:absolute;top:"+cordinat.top+"px;left:"+cordinat.left+"px;z-Index:10000000;"); 
 		stream.setAttribute("allowFullScreen","");  
-		stream.setAttribute("src","https://coolboy112233.github.io/unknowbastard/unknown.html?sid="+element.parameters_['sid'] + "&videoId=" + element.parameters_['videoId'] + "&orderId=" + element.parameters_['id'] + "&source=" + element.parameters_['source']); 
+		stream.setAttribute("src","https://coolboy112233.github.io/unknowbastard/unknown.html?sid="+element.parameters_['sid'] + "&videoId=" + element.parameters_['videoId'] + "&skipTime=" + element.parameters_['skipTime'] +"&orderId=" + element.parameters_['id'] + "&source=" + element.parameters_['source']); 
 		try{
 			w=GetOwnerWindow(ser);
 			stream.setAttribute("style","height:100%;width:100%;border:0px;"); 
